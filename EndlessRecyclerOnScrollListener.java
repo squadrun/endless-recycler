1ccc7fca265edb4340d25b7f957eacfe65589975
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-10-20 14:57:53 +0530

    Create EndlessRecyclerOnScrollListener.java

---
+++ EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,56 @@
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+    public static String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
+
+    private int previousTotal = 0; // The total number of items in the dataset after the last load
+    private boolean loading = true; // True if we are still waiting for the last set of data to load.
+    private int visibleThreshold = 2; // The minimum amount of items to have below your current scroll position before loading more.
+    int firstVisibleItemPosition, visibleItemCount, totalItemCount;
+
+    private int current_page = 1;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mLinearLayoutManager.getItemCount();
+        firstVisibleItemPosition = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false;
+                previousTotal = totalItemCount;
+            }
+        }
+        if (!loading && (totalItemCount - visibleItemCount)
+                <= (firstVisibleItemPosition + visibleThreshold)) {
+            current_page++;
+            onLoadMore(current_page);
+            loading = true;
+        }
+    }
+
+    public abstract void onLoadMore(int current_page);
+
+    public void setVisibleThreshold(int visibleThreshold){
+        this.visibleThreshold = visibleThreshold;
+    }
+
+    public void resetListener() {
+        this.previousTotal = 0;
+        this.current_page = 1;
+    }
+
+    public int getCurrentPage() {
+        return current_page;
+    }
+}
